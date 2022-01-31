a3dd221bf5e2140068f980f92b95e37b0ecc8b47
BONET <bonetprogramacion@gmail.com>
2022-01-31 13:47:33 +0100

    Initial PY LED

---
+++ 01-btn LED/py/btnLED.py
@@ -0,0 +1,51 @@
+import sys
+from tkinter import *
+from PIL import Image, ImageTk
+
+
+ws = Tk()
+
+
+def change_img(Switch):
+    if (Switch == 1):
+        img2 = ImageTk.PhotoImage(Image.open("resources/on.png"))
+        label.configure(image=img2)
+        label.image = img2
+    else:
+        img2 = ImageTk.PhotoImage(Image.open("resources/off.png"))
+        label.configure(image=img2)
+        label.image = img2
+
+
+def switchLED():
+    switchResult = 0
+    if switchButton.config('text')[-1] == 'APAGAR':
+        switchButton.config(text='ENCENDER')
+        switchResult = 0
+    else:
+        switchButton.config(text='APAGAR')
+        print("encender luz")
+        switchResult = 1
+    change_img(switchResult)
+
+
+def Comandos():
+    switchLED()
+    change_img(switchLED())
+
+
+def exitAPP():
+    sys.exit()
+
+
+img1 = ImageTk.PhotoImage(Image.open("resources/off.png"))
+label = Label(ws, image=img1)
+label.pack()
+
+ws.attributes('-fullscreen', True)
+switchButton = Button(text="ENCENDER", width=10, command=switchLED)
+switchButton.pack(pady=10)
+button = Button(ws, text="Change", command=change_img)
+ws.bind("<Return>", change_img)
+btnExit = Button(ws, text="Salir", command=exitAPP).pack()
+ws.mainloop()
